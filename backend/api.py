fda43ee8a2f03d8d91e861aa0717b0807031764e
SmoothBrainApe <bodyhealthtalks@gmail.com>
2024-08-26 16:28:44 +1000

    testing backend functions

--- backend/api.py
+++ backend/api.py
@@ -3,42 +3,46 @@ from backend.database import Database
 import datetime
 
 
-def main():
-    app = FastAPI()
+app = FastAPI()
 
-    title = input("Enter todo list title: ")
-    db = Database(title)
+title = input("Enter todo list title: ")
+db = Database(title)
 
-    todo_cache = db.extract_table()
+todo_cache = db.extract_table()
 
-    @app.post("/entries")
-    def create_entry(entry: str) -> dict[str, dict]:
-        if todo_cache:
-            next_index = max(todo_cache.keys()) + 1
-        else:
-            next_index = 1
 
-        new_entry = {
-            "id_num": next_index,
-            "entry_content": entry,
-            "date_updated": datetime.date.today(),
-        }
+@app.post("/entries")
+def create_entry(entry: str) -> dict[str, dict]:
+    if todo_cache:
+        next_index = max(todo_cache.keys()) + 1
+    else:
+        next_index = 1
 
-        todo_cache[next_index] = new_entry
-        return {"message": new_entry}
+    new_entry = {
+        "id_num": next_index,
+        "entry_content": entry,
+        "date_updated": datetime.date.today(),
+    }
 
-    @app.get("/entries")
-    def show_list() -> dict[int, dict]:
-        return todo_cache
+    todo_cache[next_index] = new_entry
+    return {"message": new_entry}
 
-    @app.get("/entries/{entry_id}")
-    def show_entry(entry_id: int) -> dict[int, dict]:
-        pass
 
-    @app.put("/entries/{entry_id}")
-    def update_entry(entry_id: int):
-        pass
+@app.get("/entries")
+def show_list() -> dict[int, dict]:
+    return todo_cache
 
-    @app.delete("/entries/{entry_id}")
-    def delete_entry(entry_id: int):
-        pass
+
+@app.get("/entries/{entry_id}")
+def show_entry(entry_id: int) -> dict[int, dict]:
+    pass
+
+
+@app.put("/entries/{entry_id}")
+def update_entry(entry_id: int):
+    pass
+
+
+@app.delete("/entries/{entry_id}")
+def delete_entry(entry_id: int):
+    pass
--- main.py
+++ main.py
@@ -1,4 +1,39 @@
-from backend.api import main
+from backend.api import create_entry, show_entry, update_entry, delete_entry
 
-if __name__ == "__main__":
-    main()
+
+while True:
+    options = {
+        "A": "Create entry",
+        "B": "View entry",
+        "C": "Update entry",
+        "D": "Delete entry",
+    }
+    print("Please choose among the following options:\n")
+    for key, value in options.items():
+        print(f"{key}. {value}")
+    prompt = input("\nEnter your option: ")
+    if prompt.lower() == "q":
+        break
+    elif prompt in options.keys():
+        if prompt.lower() == "a":
+            print("You chose create an entry.\n")
+            entry = input("Enter your entry: ")
+            create_entry(entry)
+            print("What would you like to do next?")
+        elif prompt.lower() == "b":
+            print("you want to view a certain entry?\n")
+            id_num = input("Select the id number of the one you want to view")
+            show_entry(id_num)
+            print("What would you like to do next?")
+        elif prompt.lower() == "c":
+            print("you want to update a certain entry?\n")
+            id_num = input("Select the id number of the one you want to update")
+            update_entry(id_num)
+            print("What would you like to do next?")
+        elif prompt.lower() == "d":
+            print("You chose delete an entry.\n")
+            id_num = input("Select the id number of the one you want to delete")
+            delete_entry(id_num)
+            print("What would you like to do next?")
+    else:
+        print("Invalid option. Choose again.")
