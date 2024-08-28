9b700ecda551c83ab6935dde3be64e54b4973b44
SmoothBrainApe <bodyhealthtalks@gmail.com>
2024-08-28 15:04:13 +1000

    Refactored gui.py and main.py

--- frontend/gui.py
+++ frontend/gui.py
@@ -1,276 +1,287 @@
 import tkinter as tk
 import os
 import json
-from api_requests import FrontendRequests
-
-bg = "#181818"
-fg = "#EFEFEF"
-selected_bg = "#4A4A4A"
-selected_fg = "#ffffff"
-btn_bg = "#383838"
-
-list_file = "list_items.json"
-
-window = tk.Tk()
-window.title("Todo List")
-window.geometry("1280x720")
-
-req = FrontendRequests()
-
-main_frame = tk.Frame(master=window, width=1280, height=720, bg=bg)
-main_frame.pack(fill=tk.BOTH, expand=True)
-
-window_frame = tk.Frame(master=main_frame, width=50, height=50, bg=btn_bg)
-window_frame.pack(padx=5, pady=5, fill=tk.BOTH, side=tk.TOP, expand=True)
+import signal
+import atexit
+from frontend.api_requests import FrontendRequests
 
 
-def clear_list():
-    for widget in window_frame.winfo_children():
-        if isinstance(widget, tk.Frame):
-            widget.destroy()
+class TodoListApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Todo List")
+        self.root.geometry("1280x720")
 
+        self.bg = "#181818"
+        self.fg = "#EFEFEF"
+        self.selected_bg = "#4A4A4A"
+        self.selected_fg = "#ffffff"
+        self.btn_bg = "#383838"
 
-def add_entry(text):
-    req.add_entry(text)
-    display_list()
+        self.list_file = "frontend/list_items.json"
 
+        self.req = FrontendRequests()
 
-def update_item(id_num, text):
-    req.update_entry(id_num, text)
-    display_list()
+        self.main_frame = tk.Frame(master=self.root, width=1280, height=720, bg=self.bg)
+        self.main_frame.pack(fill=tk.BOTH, expand=True)
 
+        self.window_frame = tk.Frame(
+            master=self.main_frame, width=50, height=50, bg=self.btn_bg
+        )
+        self.window_frame.pack(padx=5, pady=5, fill=tk.BOTH, side=tk.TOP, expand=True)
 
-def delete_item(id_num):
-    req.delete(id_num)
-    display_list()
+        self.button_frame = tk.Frame(
+            master=self.main_frame, width=50, height=50, bg=self.bg
+        )
+        self.button_frame.pack(padx=5, fill=tk.X, side=tk.BOTTOM)
 
+        self.list_button = tk.Button(
+            self.button_frame,
+            text="Select List Name",
+            command=self.list_names,
+            bg=self.btn_bg,
+            fg=self.fg,
+            border=0,
+            activebackground=self.selected_bg,
+            activeforeground=self.selected_fg,
+        )
+        self.list_button.pack(side=tk.RIGHT, padx=5, pady=5)
 
-def display_list():
-    clear_list()
-    get_list = req.get()
-    for key, value in get_list.items():
-        id_num = value["id_num"]
-        entry = value["entry_content"]
-        date = value["date_updated"]
+        self.list_label_var = tk.StringVar()
+        self.list_label = tk.Label(
+            self.button_frame, textvariable=self.list_label_var, bg=self.bg, fg=self.fg
+        )
+        self.list_label.pack(side=tk.RIGHT, padx=5, pady=5)
+
+        self.list_label_var.trace("w", self.monitor_label)
+
+        self.menu = tk.Menu(
+            self.root,
+            tearoff=False,
+            bg=self.btn_bg,
+            fg=self.fg,
+            relief=tk.FLAT,
+            border=0,
+            activebackground=self.selected_bg,
+            activeforeground=self.selected_fg,
+            activeborderwidth=0,
+        )
+        self.menu_item = tk.Menu(
+            self.menu,
+            tearoff=False,
+            bg=self.btn_bg,
+            fg=self.fg,
+            relief=tk.FLAT,
+            border=0,
+            activebackground=self.selected_bg,
+            activeforeground=self.selected_fg,
+            activeborderwidth=0,
+        )
+
+        self.menu_item.add_command(label="Settings", command=self.settings)
+        self.menu_item.add_command(label="Exit", command=self.exit_window)
+
+        self.menu.add_cascade(label="Options", menu=self.menu_item)
+        self.root.config(menu=self.menu)
+
+        self.root.protocol("WM_DELETE_WINDOW", self.exit_window)
+        atexit.register(self.exit_window)
+        signal.signal(signal.SIGINT, self.exit_on_signal)
+        signal.signal(signal.SIGTERM, self.exit_on_signal)
+        signal.signal(signal.SIGABRT, self.exit_on_signal)
+        signal.signal(signal.SIGSEGV, self.exit_on_signal)
+        signal.signal(signal.SIGBUS, self.exit_on_signal)
+        signal.signal(signal.SIGFPE, self.exit_on_signal)
+        signal.signal(signal.SIGILL, self.exit_on_signal)
+
+    def clear_list(self):
+        for widget in self.window_frame.winfo_children():
+            if isinstance(widget, tk.Frame):
+                widget.destroy()
+
+    def add_entry(self, text):
+        self.req.add_entry(text)
+        self.display_list()
+
+    def update_item(self, id_num, text):
+        self.req.update_entry(id_num, text)
+        self.display_list()
+
+    def delete_item(self, id_num):
+        self.req.delete(id_num)
+        self.display_list()
+
+    def display_list(self):
+        self.clear_list()
+        get_list = self.req.get()
+        for key, value in get_list.items():
+            id_num = value["id_num"]
+            entry = value["entry_content"]
+            date = value["date_updated"]
+
+            entry_frame = tk.Frame(
+                self.window_frame,
+                bg=self.bg,
+            )
+            entry_frame.pack(padx=5, pady=5, fill=tk.X, side=tk.TOP)
+
+            text_var = tk.StringVar()
+            text_var.set(f"{id_num}. {entry} - {date} (last date updated)")
+            label = tk.Label(
+                entry_frame,
+                textvariable=text_var,
+                bg=self.bg,
+                fg=self.fg,
+            )
+            label.pack(side=tk.LEFT)
+
+            update_btn = tk.Button(
+                entry_frame,
+                text="Update",
+                command=lambda id_num=id_num: self.open_entry_window(id_num),
+                bg=self.btn_bg,
+                fg=self.fg,
+                border=0,
+                activebackground=self.selected_bg,
+                activeforeground=self.selected_fg,
+            )
+            delete_btn = tk.Button(
+                entry_frame,
+                text="Delete",
+                command=lambda id_num=id_num: self.delete_item(id_num),
+                bg=self.btn_bg,
+                fg=self.fg,
+                border=0,
+                activebackground=self.selected_bg,
+                activeforeground=self.selected_fg,
+            )
+            update_btn.pack(side=tk.RIGHT, padx=5)
+            delete_btn.pack(side=tk.RIGHT, padx=5)
 
         entry_frame = tk.Frame(
-            window_frame,
-            bg=bg,
+            self.window_frame,
+            bg=self.bg,
         )
         entry_frame.pack(padx=5, pady=5, fill=tk.X, side=tk.TOP)
 
-        text_var = tk.StringVar()
-        text_var.set(f"{id_num}. {entry} - {date} (last date updated)")
-        label = tk.Label(
+        create_btn = tk.Button(
             entry_frame,
-            textvariable=text_var,
-            bg=bg,
-            fg=fg,
+            text="+",
+            command=self.open_entry_window,
+            bg=self.btn_bg,
+            fg=self.fg,
+            border=0,
+            activebackground=self.selected_bg,
+            activeforeground=self.selected_fg,
         )
-        label.pack(side=tk.LEFT)
+        create_btn.pack(pady=5, padx=5, side=tk.LEFT)
+
+    def open_entry_window(self, id_num=None):
+        entry_window = tk.Toplevel(self.root, bg=self.bg)
+        entry_window.title("Enter your Entry")
+
+        entry_label = tk.Label(
+            entry_window, text="Enter your entry here: ", bg=self.bg, fg=self.fg
+        )
+        entry_label.pack(side=tk.LEFT, padx=10, pady=5)
+
+        entry_field = tk.Entry(entry_window, bg=self.selected_bg, fg=self.fg)
+        entry_field.pack(side=tk.LEFT, pady=5)
+
+        def submit_entry():
+            entered_text = entry_field.get()
+            if id_num is None:
+                self.add_entry(entered_text)
+            else:
+                self.update_item(id_num, entered_text)
+            entry_window.destroy()
+
+        submit_button = tk.Button(
+            entry_window,
+            text="Submit",
+            command=submit_entry,
+            bg=self.btn_bg,
+            fg=self.fg,
+            border=0,
+            activebackground=self.selected_bg,
+            activeforeground=self.selected_fg,
+        )
+        submit_button.pack(side=tk.LEFT, padx=10, pady=5)
+
+    def list_names(self):
+        list_window = tk.Toplevel(self.root, bg=self.bg)
+        list_window.title("List Names")
+
+        listbox = tk.Listbox(list_window, bg=self.selected_bg, fg=self.fg)
+        listbox.pack(padx=10, pady=10)
+
+        if os.path.exists(self.list_file):
+            with open(self.list_file, "r") as f:
+                list_items = json.load(f)
+                listbox.delete(0, tk.END)
+                for item in list_items:
+                    listbox.insert(tk.END, item)
+        else:
+            list_items = []
+
+        def add_to_list():
+            item = list_entry.get().strip()
+            if item:
+                listbox.insert(tk.END, item)
+                list_entry.delete(0, tk.END)
+                list_items.append(item)
+                with open(self.list_file, "w") as f:
+                    json.dump(list_items, f)
+
+        def update_label():
+            selected_index = listbox.curselection()
+            if selected_index:
+                selected_item = listbox.get(selected_index)
+                self.list_label_var.set(selected_item)
+            list_window.destroy()
+
+        list_entry = tk.Entry(list_window, bg=self.selected_bg, fg=self.fg)
+        list_entry.pack(padx=10, pady=10)
+
+        list_btn = tk.Button(
+            list_window,
+            text="+",
+            command=add_to_list,
+            bg=self.btn_bg,
+            fg=self.fg,
+            border=0,
+            activebackground=self.selected_bg,
+            activeforeground=self.selected_fg,
+        )
+        list_btn.pack(padx=10, pady=10)
 
         update_btn = tk.Button(
-            entry_frame,
-            text="Update",
-            command=lambda id_num=id_num: open_entry_window(id_num),
-            bg=btn_bg,
-            fg=fg,
+            list_window,
+            text="Select",
+            command=update_label,
+            bg=self.btn_bg,
+            fg=self.fg,
             border=0,
-            activebackground=selected_bg,
-            activeforeground=selected_fg,
+            activebackground=self.selected_bg,
+            activeforeground=self.selected_fg,
         )
-        delete_btn = tk.Button(
-            entry_frame,
-            text="Delete",
-            command=lambda id_num=id_num: delete_item(id_num),
-            bg=btn_bg,
-            fg=fg,
-            border=0,
-            activebackground=selected_bg,
-            activeforeground=selected_fg,
-        )
-        update_btn.pack(side=tk.RIGHT, padx=5)
-        delete_btn.pack(side=tk.RIGHT, padx=5)
+        update_btn.pack(padx=10, pady=10)
 
-    entry_frame = tk.Frame(
-        window_frame,
-        bg=bg,
-    )
-    entry_frame.pack(padx=5, pady=5, fill=tk.X, side=tk.TOP)
+    def monitor_label(self, *args):
+        self.req.get_table(self.list_label_var.get())
+        self.display_list()
 
-    create_btn = tk.Button(
-        entry_frame,
-        text="+",
-        command=open_entry_window,
-        bg=btn_bg,
-        fg=fg,
-        border=0,
-        activebackground=selected_bg,
-        activeforeground=selected_fg,
-    )
-    create_btn.pack(pady=5, padx=5, side=tk.LEFT)
+    def settings(self):
+        settings_window = tk.Toplevel(self.root)
+        settings_window.title("Settings")
+        settings_window.geometry("300x200")
 
+        tk.Label(settings_window, text="Settings window").pack()
 
-button_frame = tk.Frame(master=main_frame, width=50, height=50, bg=bg)
-button_frame.pack(padx=5, fill=tk.X, side=tk.BOTTOM)
+    def exit_window(self):
+        self.req.shutdown()
+        self.root.destroy()
 
-
-def open_entry_window(id_num=None):
-    entry_window = tk.Toplevel(window, bg=bg)
-    entry_window.title("Enter your Entry")
-
-    entry_label = tk.Label(entry_window, text="Enter your entry here: ", bg=bg, fg=fg)
-    entry_label.pack(side=tk.LEFT, padx=10, pady=5)
-
-    entry_field = tk.Entry(entry_window, bg=selected_bg, fg=fg)
-    entry_field.pack(side=tk.LEFT, pady=5)
-
-    def submit_entry():
-        entered_text = entry_field.get()
-        if id_num is None:
-            add_entry(entered_text)
-        else:
-            update_item(id_num, entered_text)
-        entry_window.destroy()
-
-    submit_button = tk.Button(
-        entry_window,
-        text="Submit",
-        command=submit_entry,
-        bg=btn_bg,
-        fg=fg,
-        border=0,
-        activebackground=selected_bg,
-        activeforeground=selected_fg,
-    )
-    submit_button.pack(side=tk.LEFT, padx=10, pady=5)
-
-
-def list_names():
-    list_window = tk.Toplevel(window, bg=bg)
-    list_window.title("List Names")
-
-    listbox = tk.Listbox(list_window, bg=selected_bg, fg=fg)
-    listbox.pack(padx=10, pady=10)
-
-    if os.path.exists(list_file):
-        with open(list_file, "r") as f:
-            list_items = json.load(f)
-            listbox.delete(0, tk.END)
-            for item in list_items:
-                listbox.insert(tk.END, item)
-    else:
-        list_items = []
-
-    def add_to_list():
-        item = list_entry.get().strip()
-        if item:
-            listbox.insert(tk.END, item)
-            list_entry.delete(0, tk.END)
-            list_items.append(item)
-            with open(list_file, "w") as f:
-                json.dump(list_items, f)
-
-    def update_label():
-        selected_index = listbox.curselection()
-        if selected_index:
-            selected_item = listbox.get(selected_index)
-            list_label_var.set(selected_item)
-        list_window.destroy()
-
-    list_entry = tk.Entry(list_window, bg=selected_bg, fg=fg)
-    list_entry.pack(padx=10, pady=10)
-
-    list_btn = tk.Button(
-        list_window,
-        text="+",
-        command=add_to_list,
-        bg=btn_bg,
-        fg=fg,
-        border=0,
-        activebackground=selected_bg,
-        activeforeground=selected_fg,
-    )
-    list_btn.pack(padx=10, pady=10)
-
-    update_btn = tk.Button(
-        list_window,
-        text="Select",
-        command=update_label,
-        bg=btn_bg,
-        fg=fg,
-        border=0,
-        activebackground=selected_bg,
-        activeforeground=selected_fg,
-    )
-    update_btn.pack(padx=10, pady=10)
-
-
-list_button = tk.Button(
-    button_frame,
-    text="Select List Name",
-    command=list_names,
-    bg=btn_bg,
-    fg=fg,
-    border=0,
-    activebackground=selected_bg,
-    activeforeground=selected_fg,
-)
-list_button.pack(side=tk.RIGHT, padx=5, pady=5)
-
-list_label_var = tk.StringVar()
-list_label = tk.Label(button_frame, textvariable=list_label_var, bg=bg, fg=fg)
-list_label.pack(side=tk.RIGHT, padx=5, pady=5)
-
-
-def monitor_label(*args):
-    req.get_table(list_label_var.get())
-    display_list()
-
-
-list_label_var.trace("w", monitor_label)
-
-
-def settings():
-    settings_window = tk.Toplevel(window)
-    settings_window.title("Settings")
-    settings_window.geometry("300x200")
-
-    tk.Label(settings_window, text="Settings window").pack()
-
-
-def exit_window():
-    window.destroy()
-
-
-menu = tk.Menu(
-    window,
-    tearoff=False,
-    bg=btn_bg,
-    fg=fg,
-    relief=tk.FLAT,
-    border=0,
-    activebackground=selected_bg,
-    activeforeground=selected_fg,
-    activeborderwidth=0,
-)
-menu_item = tk.Menu(
-    menu,
-    tearoff=False,
-    bg=btn_bg,
-    fg=fg,
-    relief=tk.FLAT,
-    border=0,
-    activebackground=selected_bg,
-    activeforeground=selected_fg,
-    activeborderwidth=0,
-)
-
-menu_item.add_command(label="Settings", command=settings)
-menu_item.add_command(label="Exit", command=exit_window)
-
-
-menu.add_cascade(label="Options", menu=menu_item)
-window.config(menu=menu)
-
-window.mainloop()
+    def exit_on_signal(self, sig, frame):
+        self.req.shutdown()
+        self.root.destroy()
--- main.py
+++ main.py
@@ -1,105 +1,12 @@
-import requests
-import time
 import subprocess
+import time
+import tkinter as tk
+from frontend.gui import TodoListApp
 
 
-subprocess.Popen(["uvicorn", "api:app", "--reload"], cwd="backend/")
-
-time.sleep(2)
-
-title = input("Enter todo list title: ")
-init_response = requests.post("http://127.0.0.1:8000/tables", json={"message": title})
-
-cache = {}
-
-
-def update_list(todo_cache):
-    if not todo_cache:
-        return todo_cache
-    else:
-        todo_list = {}
-        for i, entry in enumerate(todo_cache.values(), start=1):
-            entry["id_num"] = i
-            todo_list[i] = entry
-        return todo_list
-
-
-if init_response.status_code == 200:
-    while True:
-        print("Current list of entries")
-        get_list = requests.get("http://127.0.0.1:8000/entries").json()
-        print(get_list)
-        current_list = update_list(get_list)
-        entry_list = []
-        for key, value in current_list.items():
-            for k, v in value.items():
-                entry_list.append(f"{k}: {v}, ")
-            entry_list.append("\n")
-        final_list = "".join(entry_list)
-        print(final_list)
-
-        options = {
-            "A": "Create entry",
-            "B": "View entry",
-            "C": "Update entry",
-            "D": "Delete entry",
-        }
-        print("Please choose among the following options:\n")
-        for key, value in options.items():
-            print(f"{key}. {value}")
-        prompt = input("\nEnter your option: ")
-        if prompt.lower() == "q":
-            response = requests.post("http://127.0.0.1:8000/shutdown")
-            if response.status_code == 200:
-                message = response.json()
-                print(message["message"])
-            break
-        elif prompt.upper() in options.keys():
-            if prompt.lower() == "a":
-                print("You chose create an entry.\n")
-                entry = input("Enter your entry: ")
-                response = requests.post(
-                    "http://127.0.0.1:8000/entries", json={"message": entry}
-                )
-                if response.status_code == 200:
-                    data = response.json()
-                    print(data)
-                else:
-                    print("Failed request")
-                print("What would you like to do next?")
-            elif prompt.lower() == "b":
-                print("you want to view a certain entry?\n")
-
-                id_num = int(
-                    input("Select the id number of the one you want to view: ")
-                )
-                entry_dict = requests.get(
-                    f"http://127.0.0.1:8000/entries/{id_num}"
-                ).json()
-
-                for key, value in entry_dict.items():
-                    print(f"{key}: {value}")
-
-                print("What would you like to do next?")
-            elif prompt.lower() == "c":
-                print("you want to update a certain entry?\n")
-                id_num = int(
-                    input("Select the id number of the one you want to update: ")
-                )
-                entry = input("Enter your entry update: ")
-                requests.put(
-                    f"http://127.0.0.1:8000/entries/{id_num}", json={"message": entry}
-                )
-                print("What would you like to do next?")
-            elif prompt.lower() == "d":
-                print("You chose delete an entry.\n")
-                id_num = int(
-                    input("Select the id number of the one you want to delete: ")
-                )
-                requests.delete(f"http://127.0.0.1:8000/entries/{id_num}").json()
-                print("What would you like to do next?")
-        else:
-            print("Invalid option. Choose again.")
-else:
-    print("Error has occurred")
-    exit
+if __name__ == "__main__":
+    subprocess.Popen(["uvicorn", "api:app", "--reload"], cwd="backend/")
+    time.sleep(1)
+    root = tk.Tk()
+    TodoListApp(root)
+    root.mainloop()
